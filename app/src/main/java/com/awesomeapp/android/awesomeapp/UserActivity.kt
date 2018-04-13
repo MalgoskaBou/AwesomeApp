63818650c4a283c44099d19f38204a5048d0b4cd
MalgoskaG <ghouga.m@gmail.com>
2018-04-13 23:39:17 +0200

    save students

--- app/src/main/java/com/awesomeapp/android/awesomeapp/UserActivity.kt
+++ app/src/main/java/com/awesomeapp/android/awesomeapp/UserActivity.kt
@@ -1,6 +1,7 @@
 package com.awesomeapp.android.awesomeapp
 
 import android.annotation.SuppressLint
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.view.View
@@ -16,24 +17,33 @@ import kotlin.collections.ArrayList
 import android.widget.AdapterView
 import android.widget.AdapterView.OnItemSelectedListener
 import android.widget.Toast
+import com.google.android.gms.tasks.OnSuccessListener
 import com.google.firebase.auth.FirebaseUser
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlin.collections.HashMap
 
 
 class UserActivity : MenuActivity() {
 
+    //database
     lateinit var mAuth: FirebaseAuth
     lateinit var myDatabase: FirebaseFirestore
     lateinit var mAuthStateListener: FirebaseAuth.AuthStateListener
     lateinit var myHelpData: DocumentReference
+    lateinit var myUserData: DocumentReference
 
-    val RC_SIGN_IN = 1
-
-
+    //user data
     lateinit var userName: String
     lateinit var userEmail: String
     lateinit var userUid:String
-    //lateinit var userEmailVerified: Boolean
 
+    val RC_SIGN_IN = 1
+
+    val SLACK_NAME = "slackName"
+    val LANGUAGE_1 = "languageFirst"
+    val LANGUAGE_2 = "languageSecond"
+    val CURRENT_PROJECT = "currentProject"
+    val TRACK = "userTrack"
 
 
 
@@ -47,9 +57,67 @@ class UserActivity : MenuActivity() {
 
         myDatabase = FirebaseFirestore .getInstance()
         myHelpData =  myDatabase.document("helpData/tracks")
+        mAuth = FirebaseAuth.getInstance()
 
 
         //GET DATA FROM DATABASE===================================
+        getDataFromDatabase()
+
+        //DATABASE LOG IN========================================
+        userLogIn()
+
+        //Put user data to database==============================
+        saveBtn.setOnClickListener{v ->
+            saveUser()
+        }
+
+    }
+
+    private fun saveUser() {
+        var userData  = HashMap<String, Any>()
+        userData[SLACK_NAME] = slackNick.text.toString()
+        userData[LANGUAGE_1] = lang1Spinner.selectedItem.toString()
+        userData[LANGUAGE_2] = lang2Spinner.selectedItem.toString()
+        userData[TRACK] = trackSpinner.selectedItem.toString()
+        userData[CURRENT_PROJECT] = projectsSpinner.selectedItem.toString()
+
+        myUserData.set(userData).addOnSuccessListener({
+            Toast.makeText(this@UserActivity, "Data saved", Toast.LENGTH_SHORT).show()
+
+        }).addOnFailureListener{
+            Toast.makeText(this@UserActivity, "Someting wrong :( try again", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun userLogIn(){
+
+        mAuthStateListener = FirebaseAuth.AuthStateListener { auth ->
+            val user = auth.currentUser
+            if (user != null) {
+
+                userName  = user.displayName!!
+                userEmail = user.email!!
+                userUid = user.uid
+                myUserData = myDatabase.document("Users/$userUid")
+
+                testText.text = "$userName $userEmail"
+                // User is signed in
+            } else {
+                // User is signed out
+                startActivityForResult(
+                        AuthUI.getInstance()
+                                .createSignInIntentBuilder()
+                                .setIsSmartLockEnabled(false)
+                                .setAvailableProviders(
+                                        Arrays.asList(AuthUI.IdpConfig.EmailBuilder().build(),
+                                                AuthUI.IdpConfig.GoogleBuilder().build()))
+                                .build(),
+                        RC_SIGN_IN)
+            }
+        }
+    }
+
+    private fun getDataFromDatabase(){
 
         myHelpData.addSnapshotListener(this, EventListener<DocumentSnapshot> { snapshots, e ->
             if (e != null) {
@@ -102,39 +170,7 @@ class UserActivity : MenuActivity() {
                 lang2Spinner.adapter = spinnerAdapterLanguages
             }
         })
-        //END == GET DATA FROM DATABASE==========================
-
-
-        //DATABASE LOG IN========================================
-        mAuth = FirebaseAuth.getInstance()
-
-        mAuthStateListener = FirebaseAuth.AuthStateListener { auth ->
-            val user = auth.currentUser
-            if (user != null) {
-
-                userName  = user.displayName!!
-                userEmail = user.email!!
-                //userEmailVerified = user.emailVerified
-                userUid = user.uid
-
-                testText.text = "$userName $userEmail"
-                // User is signed in
-            } else {
-                // User is signed out
-                startActivityForResult(
-                        AuthUI.getInstance()
-                                .createSignInIntentBuilder()
-                                .setIsSmartLockEnabled(false)
-                                .setAvailableProviders(
-                                        Arrays.asList(AuthUI.IdpConfig.EmailBuilder().build(),
-                                                AuthUI.IdpConfig.GoogleBuilder().build()))
-                                .build(),
-                        RC_SIGN_IN)
-            }
-        } // END == OF DATABASE LOG IN===========================
-
-    }//End onCreate
-
+    }
 
     override fun onResume() {
         super.onResume()
