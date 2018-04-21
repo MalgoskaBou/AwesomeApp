ad3c45593bbd1500c86ce9ed339676b9d29c02db
MalgoskaG <ghouga.m@gmail.com>
2018-04-21 15:00:13 +0200

    Upgrade database dependencies and Kotlin library

--- app/src/main/java/com/awesomeapp/android/awesomeapp/adapters/UserAdapter.kt
+++ app/src/main/java/com/awesomeapp/android/awesomeapp/adapters/UserAdapter.kt
@@ -1,19 +1,3 @@
-/*
- *    Copyright 2018 MalgoskaG & Bwaim
- *
- *    Licensed under the Apache License, Version 2.0 (the "License");
- *    you may not use this file except in compliance with the License.
- *    You may obtain a copy of the License at
- *
- *        http://www.apache.org/licenses/LICENSE-2.0
- *
- *    Unless required by applicable law or agreed to in writing, software
- *    distributed under the License is distributed on an "AS IS" BASIS,
- *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *    See the License for the specific language governing permissions and
- *    limitations under the License.
- */
-
 package com.awesomeapp.android.awesomeapp.adapters
 
 import android.support.v7.widget.RecyclerView
@@ -22,6 +6,10 @@ import android.view.View
 import android.view.ViewGroup
 import com.awesomeapp.android.awesomeapp.R
 import com.awesomeapp.android.awesomeapp.model.UserModel
+import kotlinx.android.synthetic.main.user_element.view.*
+import android.text.method.TextKeyListener.clear
+
+
 
 class UserAdapter (val usersList: ArrayList<UserModel>): RecyclerView.Adapter<UserAdapter.ViewHolder>() {
 
@@ -45,6 +33,7 @@ class UserAdapter (val usersList: ArrayList<UserModel>): RecyclerView.Adapter<Us
         val workOrFinished = itemView.duringWorkOrFinished
         val languages = itemView.languagesTxt
     }
+
 
 
 }
