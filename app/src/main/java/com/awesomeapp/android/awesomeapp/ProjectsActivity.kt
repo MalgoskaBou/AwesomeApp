38e5649a1cd4aa70e6b749c4604699010f2a4eed
Fabien Boismoreau <fabien.boismoreau@gmail.com>
2018-05-04 13:21:15 +0200

    feat: Refresh automatically projects Activity

--- app/src/main/java/com/awesomeapp/android/awesomeapp/ProjectsActivity.kt
+++ app/src/main/java/com/awesomeapp/android/awesomeapp/ProjectsActivity.kt
@@ -19,37 +19,37 @@ package com.awesomeapp.android.awesomeapp
 import android.os.Bundle
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
-import android.util.Log
-import android.view.View
 import android.widget.LinearLayout
-import com.awesomeapp.android.awesomeapp.R.id.*
 import com.awesomeapp.android.awesomeapp.adapters.ProjectsAdapter
 import com.awesomeapp.android.awesomeapp.data.Constant.TABLE_WITH_DATA
+import com.awesomeapp.android.awesomeapp.model.ProjectsModel
 import com.awesomeapp.android.awesomeapp.util.QueryUtils
 import kotlinx.android.synthetic.main.activity_projects.*
 import kotlinx.android.synthetic.main.toolbar_layout.*
 import org.jetbrains.anko.alert
-import org.jetbrains.anko.noButton
-import org.jetbrains.anko.toast
 import org.jetbrains.anko.yesButton
 
 
-class ProjectsActivity : MenuActivity() {
+class ProjectsActivity : MenuActivity(), Refreshable {
+
+    private var projects: ArrayList<ProjectsModel> = ArrayList()
+    private lateinit var rv: RecyclerView
+    private lateinit var track: String
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_projects)
 
         val intent = intent
-        val track = intent.getStringExtra(TABLE_WITH_DATA)
+        track = intent.getStringExtra(TABLE_WITH_DATA)
 
         myToolbar.title = track
         setSupportActionBar(myToolbar)
 
-        val rv = findViewById<RecyclerView>(R.id.projectsList)
+        rv = findViewById<RecyclerView>(R.id.projectsList)
         rv.layoutManager = LinearLayoutManager(this, LinearLayout.VERTICAL, false)
 
-        var projects = QueryUtils.getProjects(track) ?: ArrayList()
+        projects.addAll(QueryUtils.getProjects(track) ?: ArrayList())
 
         var adapter = ProjectsAdapter(projects, this)
 
@@ -73,5 +73,21 @@ class ProjectsActivity : MenuActivity() {
                 yesButton { }
             }.show()
         }
+        QueryUtils.setProjectActivity(this)
     }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        QueryUtils.removeProjectActivity()
+    }
+
+    override fun refreshUI() {
+        projects.clear()
+        rv.removeAllViews()
+        projects.addAll(QueryUtils.getProjects(track) ?: ArrayList())
+    }
+}
+
+interface Refreshable {
+    fun refreshUI()
 }
